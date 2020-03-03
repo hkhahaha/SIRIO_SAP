8f4145176ca2538df9e9d6a5be627c224f6306ca
hkhahaha <931000971@qq.com>
2020-03-03 16:05:09 +0800

    20200303

--- 其他/库存地VBA代码.vb
+++ 其他/库存地VBA代码.vb
@@ -5,11 +5,38 @@ Sub Macro1()
 Dim rng As Range, n
 n = 0
 Dim A, B, col, row
-For Each rng In [J3:R4]
+For Each rng In [J3:R102]
     If rng.Value = "√" Then
         n = n + 1
-        MsgBox (n & "哈哈" & rng.row & "列" & rng.Column)
+        'MsgBox (n & "哈哈" & rng.row & "列" & rng.Column)
+        A = rng.row
+        B = rng.Column
+        Worksheets(2).Cells(n, 1).Value = Worksheets(1).Cells(A, 2).Value
+        'Worksheets(2).Cells(n, 1).Value = Worksheets(1).Cells(A, 2).Value
+        Worksheets(2).Cells(n, 3).Value = Worksheets(1).Cells(A, 6).Value
+        Worksheets(2).Cells(n, 4).Value = Worksheets(1).Cells(A, 7).Value
+        If B = 10 Then
+        Worksheets(2).Cells(n, 2).Value = "Z010"
+        ElseIf B = 11 Then
+        Worksheets(2).Cells(n, 2).Value = "Z020"
+        ElseIf B = 12 Then
+        Worksheets(2).Cells(n, 2).Value = "Z030"
+        ElseIf B = 13 Then
+        Worksheets(2).Cells(n, 2).Value = "Z040"
+        ElseIf B = 14 Then
+        Worksheets(2).Cells(n, 2).Value = "Z050"
+        ElseIf B = 15 Then
+        Worksheets(2).Cells(n, 2).Value = "Z060"
+        ElseIf B = 16 Then
+        Worksheets(2).Cells(n, 2).Value = "Z070"
+        ElseIf B = 17 Then
+        Worksheets(2).Cells(n, 2).Value = "Z090"
+        ElseIf B = 18 Then
+        Worksheets(2).Cells(n, 2).Value = "Z080"
+        End If
+        
     End If
 Next rng
 MsgBox (n)
 End Sub
+
